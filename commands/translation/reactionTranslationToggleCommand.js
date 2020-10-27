b7e90ddc1405732ff29309163d2d3ba0fa084ceb
Christopher Polloni <christopher.polloni16@stjohns.edu>
2020-10-26 22:48:31 -0400

    save toggling translation to db

--- commands/translation/reactionTranslationToggleCommand.js
+++ commands/translation/reactionTranslationToggleCommand.js
@@ -15,27 +15,46 @@ module.exports = class translationReactionToggleCommand extends Commando.Command
     })
   }
   async run(receivedMessage, arg) {
-    if (arg == 'on' && receivedMessage.guild.translatorData.reactionTranslator == true) {
-      return receivedMessage.say(`The ability to translate messages using reactions is already enabled!`);
-    }
-    else if (arg == 'on' && receivedMessage.guild.translatorData.reactionTranslator == false) {
-      receivedMessage.guild.translatorData.reactionTranslator = true;
-      return receivedMessage.say(`The ability to translate messages using reactions is now enabled!`);
-    }
-    else if (arg == 'off' && receivedMessage.guild.translatorData.reactionTranslator == true) {
-      receivedMessage.guild.translatorData.reactionTranslator = false;
-      return receivedMessage.say(`The ability to translate messages using reactions is now disabled!`);
-    }
-    else if (arg == 'off' && receivedMessage.guild.translatorData.reactionTranslator == false) {
-      return receivedMessage.say(`The ability to translate messages using reactions is already disabled!`);
-    }
-    else {
-      if (receivedMessage.guild.translatorData.reactionTranslator == true){
-        return receivedMessage.say(`The ability to translate messages using reactions is currently enabled!`);
+    try {
+      if (arg == 'on' && receivedMessage.guild.translatorData.reactionTranslator == true) {
+        return receivedMessage.say(`The ability to translate messages using reactions is already enabled!`);
+      }
+      else if (arg == 'on' && receivedMessage.guild.translatorData.reactionTranslator == false) {
+        await upsertTranslatorSetting(receivedMessage.guild.id, { reactionTranslator: true });
+        receivedMessage.guild.translatorData.reactionTranslator = true;
+        return receivedMessage.say(`The ability to translate messages using reactions is now enabled!`);
+      }
+      else if (arg == 'off' && receivedMessage.guild.translatorData.reactionTranslator == true) {
+        await upsertTranslatorSetting(receivedMessage.guild.id, { reactionTranslator: false });
+        receivedMessage.guild.translatorData.reactionTranslator = false;
+        return receivedMessage.say(`The ability to translate messages using reactions is now disabled!`);
+      }
+      else if (arg == 'off' && receivedMessage.guild.translatorData.reactionTranslator == false) {
+        return receivedMessage.say(`The ability to translate messages using reactions is already disabled!`);
       }
       else {
-        return receivedMessage.say(`The ability to translate messages using reactions is currently disabled!`);
+        if (receivedMessage.guild.translatorData.reactionTranslator == true) {
+          return receivedMessage.say(`The ability to translate messages using reactions is currently enabled!`);
+        }
+        else {
+          return receivedMessage.say(`The ability to translate messages using reactions is currently disabled!`);
+        }
       }
+    } catch (e) {
+      console.error(e);
+      receivedMessage.say(`There was an error updating your server's translation settings. Please try again.`)
     }
+
   }
 };
+
+
+async function upsertTranslatorSetting(guildId, updatedSetting) {
+  const MongoClient = require('mongodb').MongoClient;
+  const uri = config.mongoUri;
+  const client2 = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  await client2.connect();
+  result = await client2.db("DiscordBot").collection("Translator Settings").updateOne({ guildId: guildId }, { $set: updatedSetting }, { upsert: true });
+  await client2.close();
+  return
+}
