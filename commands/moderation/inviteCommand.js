e1d031ee04266677619fe419a62a491242ad8751
Christopher Polloni <christopher.polloni16@stjohns.edu>
2020-11-06 00:48:17 -0500

    add bot/server invite commands

---
+++ commands/moderation/inviteCommand.js
@@ -0,0 +1,36 @@
+const Commando = require('discord.js-commando');
+const path = require('path');
+const config = require('../../config.js');
+
+module.exports = class roleCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'invite',
+            group: 'moderation',
+            memberName: 'invite',
+            description: 'Generate an invite for this server',
+            examples: [`invite`],
+            guildOnly: true,
+            argsType: 'single',
+            userPermissions: ['CREATE_INSTANT_INVITE']
+        })
+    }
+    async run(receivedMessage) {
+        let guild = receivedMessage.guild;
+        let channel = guild.systemChannel;
+        if (!channel) {
+            channel = guild.channels.cache.last();
+        }
+        return createLink(receivedMessage, channel, guild);
+    }
+};
+
+async function createLink(receivedMessage, channel, guild) {
+    let invite = await channel.createInvite({ maxAge: 0, unique: false }).catch(console.error);
+    try {
+        receivedMessage.author.send(`${guild.name} | ${invite}`);
+        receivedMessage.say(`Invite was sent to your DM!`);
+    } catch (e) {
+        receivedMessage.say(`${guild.name} | no link available`);
+    }
+}
