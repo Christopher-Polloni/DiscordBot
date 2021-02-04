ef61a7b4e145cc32382916e105539640c576c0eb
Christopher-Polloni <pollonichristopher@gmail.com>
2021-02-04 11:39:47 -0500

    add casino credit command

---
+++ commands/casino/creditsCommand.js
@@ -0,0 +1,28 @@
+const Commando = require('discord.js-commando');
+const path = require('path');
+const config = require('../../config.js');
+const Discord = require('discord.js');
+
+module.exports = class creditsCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'credits',
+            aliases: ['credit', 'balance'],
+            group: 'casino',
+            memberName: 'credits',
+            description: 'View how many casino credits you have.',
+            examples: ['credits'],
+            guildOnly: false,
+            argsType: 'single',
+        })
+    }
+    async run(receivedMessage, args) {
+        if (!receivedMessage.author.casino.setup) {
+            return receivedMessage.say('You must first set up your casino account before using any casino commands. To do this, simply run the `casino-setup` command.')
+        }
+        else {
+            return receivedMessage.say(`You currently have ${receivedMessage.author.casino.balance.toLocaleString()} credits.`)
+        }
+
+    }
+};
