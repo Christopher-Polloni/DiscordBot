ba716faa56d48f285dce7789bccbb937ddf5e178
Christopher-Polloni <pollonichristopher@gmail.com>
2021-02-24 21:06:54 -0500

    slap image manipulation command added

---
+++ commands/image manipulation/slapCommand.js
@@ -0,0 +1,34 @@
+const Commando = require('discord.js-commando');
+const path = require('path');
+const config = require('../../config.js');
+const Discord = require('discord.js');
+const imageManipulationFunctions = require('../../util/imageManipulation');
+
+module.exports = class slapCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'slap',
+            group: 'imagemanipulation',
+            memberName: 'slap',
+            description: 'Sends a Batman/Robin slap meme with your avatar\'s image as Batman and an image of your choice or another user\'s avatar as Robin.',
+            examples: ['slap <@UserMention>', 'slap <image>'],
+            guildOnly: false,
+            argsType: 'single'
+        })
+    }
+    async run(receivedMessage, arg) {
+        const batman = receivedMessage.author.displayAvatarURL({ format: 'png' })
+        if (receivedMessage.mentions.members.first()){
+            return imageManipulationFunctions.slapImage(receivedMessage, batman, receivedMessage.mentions.members.first().user.displayAvatarURL({ format: 'png' }))
+        }
+        else if (receivedMessage.attachments.first()) {
+            return imageManipulationFunctions.slapImage(receivedMessage, batman, receivedMessage.attachments.first().url)
+        }
+        else {
+            return receivedMessage.say('You must mention another user to slap or send an image with the command.')
+        }
+     
+        
+        
+    }
+};
---
+++ util/imageManipulation.js
@@ -0,0 +1,18 @@
+const Canvacord = require("canvacord").Canvas;
+const Discord = require('discord.js');
+
+exports.slapImage = async (receivedMessage, img1, img2) => {
+
+    const batman = await Canvacord.circle(img1)
+    const robin = await Canvacord.circle(img2)
+    const img = await Canvacord.slap(batman, robin)
+
+    if (img) {
+        const attachment = new Discord.MessageAttachment(img);
+        return receivedMessage.say(attachment);
+    }
+    else {
+        return receivedMessage.say('There was an error creating the image. If using an image instead of mentioning a user, try to use a `.png` or `.jpg` file.')
+    }
+
+}
