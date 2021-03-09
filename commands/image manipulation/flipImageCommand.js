4931c34685153b02c55f963a0af939adfd3c7c5e
Christopher-Polloni <pollonichristopher@gmail.com>
2021-03-08 21:50:42 -0500

    edit to no image response for flip command

--- commands/image manipulation/flipImageCommand.js
+++ commands/image manipulation/flipImageCommand.js
@@ -27,7 +27,7 @@ module.exports = class flipImageCommand extends Commando.Command {
         let type = args[0]
         if (type) type = type.toLowerCase()
         if (!args || !(horizontal.includes(type) || vertical.includes(type) || both.includes(type))) {
-            return receivedMessage.say('Proper usage of this command requires the type of flip you\'d like to image to undergo.\n```flip-image <type> <@UserMention>\nflip-image <type> <image>\nflip-image <type>```\nTo flip horizontally, the type would be: `h`, `horizontal` or `horizontally`\nTo flip vertically, the type would be: `v`, `vertical` or `vertically`\nTo flip both horizontally and vertically, the type would be: `h/v`, `h+v` or `both`\nExamples:\nflip-image horizontally <@575416249400426506>\nflip-image v <@575416249400426506>\nflip-image h+v <@575416249400426506>')
+            return receivedMessage.say('Proper usage of this command requires the type of flip you\'d like the image to undergo.\n```flip-image <type> <@UserMention>\nflip-image <type> <image>```\nTo flip horizontally, the type would be: `h`, `horizontal` or `horizontally`\nTo flip vertically, the type would be: `v`, `vertical` or `vertically`\nTo flip both horizontally and vertically, the type would be: `h/v`, `h+v` or `both`\nExamples:\nflip-image horizontally <@575416249400426506>\nflip-image v <@575416249400426506>\nflip-image h+v <@575416249400426506>')
         }
         if (receivedMessage.mentions.members.first()) {
             return imageManipulationFunctions.flipImage(receivedMessage, receivedMessage.mentions.members.first().user.displayAvatarURL({ format: 'png' }), getFlips(type))
@@ -36,7 +36,7 @@ module.exports = class flipImageCommand extends Commando.Command {
             return imageManipulationFunctions.flipImage(receivedMessage, receivedMessage.attachments.first().url, getFlips(type))
         }
         else {
-            return receivedMessage.say("You must provide the image to be used in this command. Run the command again and either mention the user who's avatar you'd like to be used or send an image at the same time.\nExample: flip-image <@575416249400426506>")
+            return receivedMessage.say('Proper usage of this command requires the type of flip you\'d like the image to undergo.\n```flip-image <type> <@UserMention>\nflip-image <type> <image>```\nTo flip horizontally, the type would be: `h`, `horizontal` or `horizontally`\nTo flip vertically, the type would be: `v`, `vertical` or `vertically`\nTo flip both horizontally and vertically, the type would be: `h/v`, `h+v` or `both`\nExamples:\nflip-image horizontally <@575416249400426506>\nflip-image v <@575416249400426506>\nflip-image h+v <@575416249400426506>')
         }
     }
 };
